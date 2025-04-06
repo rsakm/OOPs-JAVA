7a1691fde2be5f44ed185e3accaf0ed3a8ae111e
Rajshree <rajshreeakm@gmail.com>
2025-04-06 18:07:42 +0530

    static blocks

---
+++ src/oops_staticKeyword/staticBlock/Test.java
@@ -0,0 +1,11 @@
+package oops_staticKeyword.staticBlock;
+
+public class Test {
+    static {
+        System.out.println("Static block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+}
