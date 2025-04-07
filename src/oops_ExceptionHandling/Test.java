810f2947e794113519a4ea42dbeb8e1203dc4669
Rajshree <rajshreeakm@gmail.com>
2025-04-07 20:39:58 +0530

    Exception

---
+++ src/oops_ExceptionHandling/Test.java
@@ -0,0 +1,13 @@
+package oops_ExceptionHandling;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println(100/25);
+        System.out.println(100/0);   /// program stops here by throwing an exception
+        System.out.println(5);
+
+    }
+}
