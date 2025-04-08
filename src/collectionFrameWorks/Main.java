46840a8e50e287b5e92426e4e64650424c2fd3a2
Rajshree <rajshreeakm@gmail.com>
2025-04-08 23:10:31 +0530

    Collection FrameWorks in Java

---
+++ src/collectionFrameWorks/Main.java
@@ -0,0 +1,13 @@
+package collectionFrameWorks;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(23);
+        list.add(34);
+        list.add(43);
+        System.out.println(list.toString());
+    }
+}
